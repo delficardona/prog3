83aab8c612b6e196e207be93a7ef0d3148e75386
Delfina Cardona <dcardona@udesa.edu.ar>
2021-09-06 11:56:34 -0300

    se ve en local host

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header'
-import Main from './Components/Main/Main'
+import Card from './Components/Card/Card'
 import Footer from './Components/Footer/Footer'
 
 function App() {
