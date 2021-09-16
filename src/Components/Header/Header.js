1bd4d91bb0656000106cfc7e47abe5bad526c736
luisanazar <nazaranchorenal@udesa.edu.ar>
2021-09-16 10:06:24 -0300

    cambio logo

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -27,8 +27,7 @@ controlarCambios(event){
             <React.Fragment>
                 <header>
                     <section className="header">
-                        <img src="./images/logo.png" alt="logo"></img>
-                        <h1> LD MOVIES</h1>
+                        <img className="logo" src="./images/logo2.png" alt="logo"></img>
                          <form action="" class="uk-search uk-search-default" onSubmit={(event)=>this.preventSubmit(event)}>
                             <span class="uk-search-icon-flip" uk-search-icon></span>
                             <input class="uk-search-input" type="search" name="search" id="" placeholder="Buscar Pelicula" onChange={(e)=>this.controlarCambios(e)} value={this.state.filterBy} />
